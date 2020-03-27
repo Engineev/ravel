ea5d0cc8a35c6a5bee06e6dea238f2ca16fdc9d1
Engineev <ryw_pub@126.com>
2020-03-27 17:32:42 +0800

    fixed a bug on the calling convention

--- src/interpreter/interpreter.cpp
+++ src/interpreter/interpreter.cpp
@@ -292,8 +292,8 @@ void Interpreter::interpret() {
       simulateLibCFunc(libc::Func(pc));
       pc = regs[1];
       // force the calling convention
-      int callerSaved[] = {1,  5,  6,  7,  10, 11, 12, 13,
-                           14, 15, 16, 17, 28, 29, 30, 31};
+      int callerSaved[] = {1,  5,  6,  7,  /* 10, */ 11, 12, 13, 14,
+                           15, 16, 17, 28, 29,           30, 31};
       for (auto reg : callerSaved)
         regs[reg] += 0x1234;
       continue;
